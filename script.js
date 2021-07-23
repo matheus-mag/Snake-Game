b712a38352f391bbd7c9c547836e0a41c2104c6d
matheus-mag <matheusmag1103@gmail.com>
2021-07-23 19:19:33 -0300

    criando a movimentação

--- script.js
+++ script.js
@@ -6,18 +6,40 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 } 
+let move = "right";
 
 function backGround(){
     context.fillStyle = "navy";
     context.fillRect(0,0, 16*box,16*box);
 }
 
-function lilsnake(){
+function cobrinha(){
     for(i=0;i<snake.length;i++){
         context.fillStyle = "greenyellow";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-backGround();
-lilsnake();
+function iniciarJogo(){
+    backGround();
+    cobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(move == "right") snakeX += box;
+    if(move == "left") snakeX -= box;
+    if(move == "up") snakeY -= box;
+    if(move == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 125);
+
